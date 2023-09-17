97c2509ca48696092e807704ebb3cbd1c4c67752
MAIL479 <mail479@icloud.com>
2023-09-17 14:41:43 +0300

    test

---
+++ Home work 22/home_work/app/notes_app.py
@@ -0,0 +1,52 @@
+class Note:
+    def __init__(self, content):
+        self.content = content
+
+
+class NotesApp:
+    def __init__(self):
+        self.notes_list = []
+
+    def add_note(self, content):
+        new_note = Note(content)
+        self.notes_list.append(new_note)
+        return "Note added successfully"
+
+    def get_note(self, index):
+        try:
+            return self.notes_list[index].content
+        except IndexError:
+            return "Index out of range"
+
+    def edit_note(self, index, content):
+        try:
+            self.notes_list[index].content = content
+            return "Note edited successfully"
+        except IndexError:
+            return "Index out of range"
+
+    def delete_note(self, index):
+        try:
+            del self.notes_list[index]
+            return 'Note deleted successfully'
+        except IndexError:
+            return 'Index out of range'
+
+
+notes_app = NotesApp()
+
+# add note
+notes_app.add_note('Test first note')
+notes_app.add_note('Test second note')
+
+# show note
+print(notes_app.get_note(0))
+print(notes_app.get_note(1))
+
+# edit note
+notes_app.edit_note(0, 'Edit first note')
+print(notes_app.get_note(0))
+
+# delete note
+notes_app.delete_note(1)
+print(notes_app.get_note(1))
